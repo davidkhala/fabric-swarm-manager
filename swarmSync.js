902e31b44502e970aa3bb0362c0f8e8993c6b005
davidliu <david-khala@hotmail.com>
2018-07-03 11:45:40 +0800

    massive refactor is required since deploy service from worker is not supported

--- swarmSync.js
+++ swarmSync.js
@@ -1,20 +1,26 @@
-//TODO send manager info to swarm Server
 const swarmClient = require('./swarmClient');
 const {fabricImagePull, swarmIPJoin} = require('./common/nodejs/fabric-dockerode');
 const {swarmLeave, swarmBelongs} = require('./common/docker/nodejs/dockerode-util');
 const dockerCmdUtil = require('./common/docker/nodejs/dockerCmd');
 const arch = 'x86_64';
 const logger = require('./common/nodejs/logger').new('swarmSync');
+const role = process.env.role?process.env.role: 'manager';
 const asyncTask = async (action) => {
 	await swarmClient.touch();
-	const {managerToken} = await swarmClient.leader();
-	const {token: JoinToken, AdvertiseAddr} = await dockerCmdUtil.advertiseAddr(managerToken);
+	const {workerToken,managerToken} = await swarmClient.leader();
+	const token = role==='manager'?managerToken:workerToken
+	const {token: JoinToken, AdvertiseAddr} = await dockerCmdUtil.advertiseAddr(token);
 
 
 	if (action === 'down') {
-		const {result} = await swarmBelongs(undefined, JoinToken);
+		const {result, swarm} = await swarmBelongs(undefined, JoinToken);
 		if (!result) {
-			await swarmLeave();
+			if (swarm) {
+				logger.warn('swarm not belongs to', JoinToken, ',but belongs to', swarm.ID);
+				await swarmLeave();
+			} else {
+				logger.warn('maybe a swarm worker');
+			}
 		}
 		logger.info('[done] down');
 		return;
@@ -22,8 +28,8 @@ const asyncTask = async (action) => {
 	const {docker: {network, thirdPartyTag, fabricTag}, TLS} = await swarmClient.globalConfig();
 	await fabricImagePull({fabricTag, thirdPartyTag, arch});
 	await swarmIPJoin({AdvertiseAddr, JoinToken});
-	const node = await dockerCmdUtil.nodeSelf(true);
-	logger.info('this node', node);
+	const node = await dockerCmdUtil.swarmWorkerInfo();
+	logger.info('this node', JSON.stringify(node));
 
 
 };
